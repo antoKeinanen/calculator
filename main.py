8b2dc5ef5055ba90c1e4fc091381f1eca9e7d4f4
BuildTools <unconfigured@null.spigotmc.org>
2021-02-18 21:18:12 +0200

    bugs

--- main.py
+++ main.py
@@ -1,7 +1,12 @@
 from kivy.app import App
+from kivy.lang import Builder
 from kivy.uix.gridlayout import GridLayout
 from kivy.config import Config
 from kivy.uix.screenmanager import ScreenManager, Screen
+from kivy.uix.screenmanager import NoTransition
+
+
+Builder.load_file('calculator.kv')
 
 Config.set('graphics', 'resizable', '0')
 Config.set('graphics', 'width', '400')
@@ -12,7 +17,17 @@ shift = False
 alpha = False
 
 
-class Normal(GridLayout):
+class Normal(Screen):
+    def calculate(self, item, calculation):
+        if calculation:
+            try:
+                item.ids.entry.text = str(eval(calculation))
+            except Exception as e:
+                print(e)
+                item.ids.entry.text = "Error"
+
+
+class Shift(Screen):
     def calculate(self, calculation):
         if calculation:
             try:
@@ -22,14 +37,16 @@ class Normal(GridLayout):
                 self.display.text = "Error"
 
 
-class CalculatorApp(App):
+class TestApp(App):
     def build(self):
+        # Create the screen manager
         sm = ScreenManager()
+        sm.transition = NoTransition()
         sm.add_widget(Normal(name="normal"))
-        sm.add_widget()
+        sm.add_widget(Shift(name="shift"))
+
         return sm
 
 
-if __name__ == "__main__":
-    CalculatorApp().run()
-
+if __name__ == '__main__':
+    TestApp().run()
